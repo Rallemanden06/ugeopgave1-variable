ecaee7de247a6dab2ca481f8c6f401f05fd263ef
Rallemanden06 <rnl06@outlook.dk>
2026-09-02 14:09:18 +0200

    første commit

---
+++ src/Main.java
@@ -0,0 +1,93 @@
+public class Main {
+    void main() {
+
+//opgave 1
+
+
+
+
+
+//opgave 2
+
+        /*
+        udkommenteret
+        */
+
+//opgave 3
+
+        /*
+        udkommenteret
+        */
+
+//opgave 4
+
+        /*
+        udkommenteret
+        */
+
+//opgave 5
+
+        /*
+        udkommenteret
+        */
+
+//opgave 6
+
+        /*
+        udkommenteret
+        */
+
+//opgave 7
+
+        /*
+        udkommenteret
+        */
+
+//opgave 8
+
+        /*
+        udkommenteret
+        */
+
+//opgave 9
+        /*
+        udkommenteret
+        */
+
+//opgave 10
+
+        /*
+        udkommenteret
+        */
+
+//opgave 11
+
+        /*
+        udkommenteret
+        */
+
+//opgave 12
+
+        /*
+        udkommenteret
+        */
+
+//opgave 13
+
+        /*
+        udkommenteret
+        */
+
+//opgave 14
+
+        /*
+        udkommenteret
+        */
+
+//opgave 15
+        /*
+        udkommenteret
+        */
+
+    }
+}
